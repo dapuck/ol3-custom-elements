3082aec2150a16be1d70b0ef5e1759d400374dd3
Ian McCall <imccall@da-puck.com>
2016-02-14 17:47:51 -0600

    Initial commit

---
+++ ol3-custom-elements.js
@@ -0,0 +1,8 @@
+/*jshint browser: true*/
+(function () {
+    var ol3MapElProto = Object.create(HTMLElement.prototype);
+    var ol3MapEl = document.registerElement('ol3-map', {
+        prototype: ol3MapElProto
+    });
+    
+})();
